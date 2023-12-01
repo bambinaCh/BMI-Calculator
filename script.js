d20191b1384e582c38394807d75f3fc91af23b54
bambinaCh <chamaa.jarite@gmail.com>
2023-12-01 14:22:47 +0100

    erweiterung js code

--- script.js
+++ script.js
@@ -1,33 +1,72 @@
 function calculateBMI() {
   var weight = parseFloat(document.getElementById('weight').value);
   var height = parseFloat(document.getElementById('height').value);
+  var gender = document.getElementById('gender').value;
+  var age = parseInt(document.getElementById('age').value);
 
-  if (isNaN(weight) || isNaN(height) || height === 0) {
+  if (isNaN(weight) || isNaN(height) || isNaN(age) || height === 0) {
     alert('Bitte geben Sie gültige Werte ein.');
     return;
   }
 
-  // Convert height to meters from centimeters
+  //meter zu centis
   var heightInMeters = height / 100;
 
-  var bmi = weight / (heightInMeters * heightInMeters);
-  displayResult(bmi);
+  var bmi;
+  if (gender === 'male') {
+    bmi = calculateBMIMale(weight, heightInMeters, age);
+  } else {
+    bmi = calculateBMIFemale(weight, heightInMeters, age);
+  }
+
+  displayResult(weight, height, age, bmi);
 }
 
-function displayResult(bmi) {
+function calculateBMIMale(weight, height, age) {
+  // mönner
+  return weight / (height * height) + age / 10;
+}
+
+function calculateBMIFemale(weight, height, age) {
+  // frauen
+  return weight / (height * height) + age / 10;
+}
+
+function displayResult(weight, height, age, bmi) {
   var resultDiv = document.getElementById('result');
   var resultText;
+  var color;
 
   if (bmi < 18.5) {
     resultText = 'Untergewicht';
-    resultDiv.style.color = '#e74c3c';
+    color = '#e74c3c';
   } else if (bmi >= 18.5 && bmi <= 24.9) {
     resultText = 'Normalgewicht';
-    resultDiv.style.color = '#2ecc71';
+    color = '#2ecc71';
   } else {
     resultText = 'Übergewicht';
-    resultDiv.style.color = '#e74c3c';
+    color = '#e74c3c';
   }
 
-  resultDiv.innerHTML = 'Ihr BMI: ' + bmi.toFixed(2) + ' - ' + resultText;
+
+  var tableHTML =
+    `<table><tr><td>Gewicht</td><td>${weight} kg</td></tr>` +
+    `<tr><td>Grösse</td><td>${height} cm</td></tr>` +
+    `<tr><td>Geschlecht</td><td>${gender}</td></tr>` +
+    `<tr><td>Alter</td><td>${age} Jahre</td></tr>` +
+    `<tr><td>BMI</td><td>${bmi.toFixed(2)}</td></tr></table>`;
+
+  resultDiv.innerHTML = tableHTML + '<br>' + 'Ihr BMI: ' + bmi.toFixed(2) + ' - ' + resultText;
+  resultDiv.style.color = color;
+}
+
+function resetForm() {
+  document.getElementById('bmiForm').reset();
+  document.getElementById('result').innerHTML = '';
+}
+
+function handleEnter(event) {
+  if (event.key === 'Enter') {
+    calculateBMI();
+  }
 }
